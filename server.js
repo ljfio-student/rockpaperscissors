c4ed5d565da5cee856599eba30693fe68f05dfca
Luke Fisher <hysteriax23@gmail.com>
2013-12-11 20:11:13 +0000

    Initial code

--- server.js
+++ server.js
@@ -1,159 +1,393 @@
-#!/bin/env node
-//  OpenShift sample Node application
-var express = require('express');
-var fs      = require('fs');
+// HTTP Server
+var fs = require('fs');
+var mo = require('./mo');
+var http = require("http");
+var url = require('url');
+var engine = require('engine.io')
+var async = require("async");
 
+// Calculate size of an object
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+      if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+// Web server
+var httpServer = http.createServer(function(req, res){
+  var uri = url.parse(req.url, true);
+  var data = "";
 
-    //  Scope.
-    var self = this;
+  var files = [
+    {path: "/", render: "static/index.html", type:"text/html"},
+    {path: "/engine.io.js", file: "static/engine.io.js", type: "text/javascript"},
+    {path: "/main.js", file: "static/main.js", type: "text/javascript"},
+    
+    {path: "/img/scissors.gif", file: "static/350x350.gif", type:"image/gif"},
+    {path: "/img/paper.gif", file: "static/350x350.gif", type:"image/gif"},
+    {path: "/img/stone.gif", file: "static/350x350.gif", type:"image/gif"}
+  ];
 
+  // Get the data being sent to us
+  req.addListener("data", function(chunk){
+    data += chunk;
+  });
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
-
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
+  req.addListener("end", function(){
+    var args = {
+      type: "",
+      status: 500,
+      content: "",
+      set: false
     };
 
-
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
-
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
+    for(var i = 0; i < files.length; i++) {
+      if(files[i].path == uri.pathname) {
+        var a = {
+          port: process.env.PORT
         };
 
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
+        args.type = files[i].type;
+        if(files[i].render)
+          args.content = mo.render(fs.readFileSync(files[i].render, 'utf8'), a);
+        else
+          args.content = fs.readFileSync(files[i].file);
+        
+        args.status = 200;
+        args.set = true;
 
+        break;
+      }
+    }
 
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
+    // If doesn't exist
+    if(!args.set) {
+      args.type = "text/plain";
+      args.content = "Page does not exist";
+      args.status = 404;
+    }
 
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
+    // Send response
+    res.writeHead(args.status, { 'Content-Type': args.type});
+    res.write(args.content);
+    res.end();
+  });
+}).listen(process.env.PORT, process.env.IP);
+
+// Game server
+var games = [];
+var games_count = 0;
+var lobby = [];
+
+// Async updates
+var updates_in = [];
+var updates_out = [];
+
+// Users
+var users = [];
+var user_space = [];
+var user_games = 50;
+var user_count = 0;
+var user_limit = user_games * 2;
+
+// Actions
+var actions = {
+  PLAY: 0,
+  SELECT: 1,
+  MAX_USERS: 2,
+  RESULT: 3,
+  GAME_START: 4,
+  IN_LOBBY: 5,
+  PLAY_AGAIN: 6,
+  DISCONNECT: 7
+};
+var weapons = {
+  PAPER: 1,
+  SCISSORS: 2,
+  STONE: 3
+};
+
+// Solves poor serializing
+function sendClient(socket, obj){
+  socket.send(JSON.stringify(obj));
+}
+
+// Game check winner
+function checkWinner(one, two) {
+  if(one == two) {
+    return 0;
+  } else if((one == weapons.SCISSORS && two == weapons.PAPER) || (one == weapons.STONE && two == weapons.SCISSORS) || (one == weapons.PAPER && two == weapons.STONE)) {
+    return 1;
+  } else {
+    return 2;
+  }
+}
+
+// Initialise the server
+var server = engine.attach(httpServer);
+server.on('connection', function (socket) {
+  socket.id = -1;
+
+  socket.on('message', function (data) {
+    if(typeof(data) == "string") {
+      var obj = JSON.parse(data);
+
+      updates_in.push({
+        id: socket.id,
+        action: obj.action,
+        value: obj.value
+      });
+    }
+  });
+
+  socket.on('close', function () {
+    socket.id = -2;
+  });
+
+  if(user_space.length > 0) {
+    socket.id = user_space.pop();
+  } else if(users.length >= user_limit) {
+    sendClient(socket, {
+      action: actions.MAX_USERS
+    });
+    socket.close();
+  } else {
+    socket.id = user_count;
+    user_count++;
+  }
+  
+  if(socket.id >= 0) {
+    users.push(socket);
+  }
+  
+  socket.old_id = socket.id;
+});
+
+// Check for updates
+async.forever(function(callback){
+  while(updates_in.length > 0) {
+    var u = updates_in.pop();
+
+    switch(u.action) {
+      case actions.PLAY:
+        if(lobby.length > 0) {
+          var opp = lobby.pop();
+
+          games.push({
+            id: games_count,
+            one: opp,
+            two: u.id,
+            s_one: 0, // Scores
+            s_two: 0,
+            c_one: 0, // Choices
+            c_two: 0,
+            r_one: false, // Ready to play
+            r_two: false,
+            times: 0
+          });
+          games_count++;
+          
+          updates_out.push({
+            to: [opp, u.id],
+            action: actions.GAME_START
+          });
+        } else {
+          lobby.push(u.id);
+          updates_out.push({
+            to: u.id,
+            action: actions.IN_LOBBY
+          });
         }
-    };
+        break;
+      case actions.SELECT:
+        for(var g in games){
+          if(u.value > 0 && u.value < (Object.size(weapons) + 1)) {
+            if(games[g].one == u.id){
+              games[g].c_one = u.value;
+            } else if(games[g].two == u.id) {
+              games[g].c_two = u.value;
+            }
 
+            // Time to compare choices
+            if(games[g].c_one !== 0 && games[g].c_two !== 0) {
+              var r = checkWinner(games[g].c_one, games[g].c_two);
 
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
+              // Reset and update stats
+              if(r == 1){
+                games[g].s_one++;
+              } else if(r == 2) {
+                games[g].s_two++;
+              }
 
-        // Create the express server and routes.
-        self.initializeServer();
-    };
+              games[g].c_one = 0;
+              games[g].c_two = 0;
+              games[g].times++;
 
+              // Push result to clients
+              updates_out.push({
+                to: games[g].one,
+                action: actions.RESULT,
+                value:{
+                  won: r == 1,
+                  drawn: r === 0,
+                  lost: r == 2,
+                  scores: {
+                    you: games[g].s_one,
+                    opp: games[g].s_two
+                  }
+                }
+              });
+              updates_out.push({
+                to: games[g].two,
+                action: actions.RESULT,
+                value: {
+                  won: r == 2,
+                  drawn: r === 0,
+                  lost: r == 1,
+                  scores: {
+                    you: games[g].s_two,
+                    opp: games[g].s_one
+                  }
+                }
+              });
+            }
+            
+            console.log("game: " + JSON.stringify(games[g]));
+          } else {
+            // Invalid choice
+            break;
+          }
+        }
+        break;
+      case actions.PLAY_AGAIN:
+        for(var g in games){
+          var game_up = false;
+          
+          if(games[g].one == u.id) {
+            games[g].r_one = true;
+            game_up = true;
+          } else if(games[g].two == u.id) {
+            games[g].r_two = true;
+            game_up = true;
+          }
+          
+          if(game_up) {
+            if(games[g].r_one && games[g].r_two) {
+              updates_out.push({
+                to: [games[g].one, games[g].two],
+                action: actions.GAME_START
+              });
+              
+              // Reset ready state
+              games[g].r_one = false;
+              games[g].r_two = false;
+            } else {
+              updates_out.push({
+                to: u.id,
+                action: actions.IN_LOBBY
+              });
+            }
+          }
+        }
+    }
 
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
+    console.log("in: " + JSON.stringify(u));
+  }
 
-};   /*  Sample Application.  */
+  setTimeout(callback, 100);
+}, function(err){
+  console.log("in-err:" + err);
+});
 
+// This sends data out to client
+async.forever(function(callback){
+  while(updates_out.length > 0){
+    var u = updates_out.pop();
+    var to = [];
+    var sent = 0;
 
+    // Make into an array
+    if(typeof u.to == "number")
+      to.push(u.to)
+    else if(u.to instanceof Array)
+      to = u.to;
 
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
+    // Loop through the 'to' list
+    for(var x in to) {
+      var t = to[x];
 
+      for(var a in users) {
+        if(users[a].id == t) {
+          sendClient(users[a], {
+            action: u.action,
+            value: u.value
+          });
+          sent++;
+
+          break;
+        }
+      }
+    }
+
+    console.log("out: " + JSON.stringify(u) + " - Sent: " + (sent == to.length ? "true" : "false"));
+  }
+
+  setTimeout(callback, 100);
+}, function(err){
+  console.log("out-err:" + err);
+});
+
+// This checks for dead connections
+async.forever(function(callback){
+  for(var x in users) {
+    if(users[x].id == -2) {
+      for(var g in games){
+        var user_disconnect = false;
+        
+        if(games[g].one == users[x].old_id) {
+          updates_out.push({
+            to: games[g].two,
+            action: actions.DISCONNECT
+          });
+          user_disconnect = true;
+        } else if(games[g].two == users[x].old_id) {
+          updates_out.push({
+            to: games[g].one,
+            action: actions.DISCONNECT
+          });
+          user_disconnect = true;
+        }
+        
+        if(user_disconnect) {
+          if(!fs.exists('games.csv')) {
+            fs.writeFileSync('games.csv', "id,one,two,times\n");
+          }
+          
+          fs.appendFileSync('games.csv',
+            games[g].id + "," +
+            games[g].s_one + "," +
+            games[g].s_two + "," +
+            games[g].times + '\n'
+          );
+          
+          games.splice(g, 1);
+        }
+      }
+      
+      for(var l in lobby) {
+        if(users[x].old_id == lobby[l]) {
+          lobby.splice(l, 1);
+        }
+      }
+      
+      user_space.push(users[x].old_id);
+      users.splice(x, 1);
+    }
+  }
+
+  setTimeout(callback, 100);
+}, function(err){
+  console.log("dead-err:" + err);
+});
---
+++ static/main.js
@@ -0,0 +1,139 @@
+$(document).ready(function(){
+  var actions = {
+    PLAY: 0,
+    SELECT: 1,
+    MAX_USERS: 2,
+    RESULT: 3,
+    GAME_START: 4,
+    IN_LOBBY: 5,
+    PLAY_AGAIN: 6,
+    DISCONNECT: 7
+  };
+  var weapons = {
+    PAPER: 1,
+    SCISSORS: 2,
+    STONE: 3
+  };
+
+  var socket;
+
+  // Solves poor serializing
+  function sendServer(socket, obj){
+    socket.send(JSON.stringify(obj));
+  }
+
+  function connectServer() {
+    var loc = location.hostname;
+      socket = eio(location.protocol == "http:" ? "ws://" : "wss://" + loc);
+      socket.onopen = function(){
+        socket.onmessage = function(data){
+          var obj = JSON.parse(data);
+
+          console.log(data);
+
+          switch(obj.action) {
+            case actions.IN_LOBBY:
+              $("#wstart").addClass("hidden");
+              $("#winterval").html("<h4>Waiting for other player...</h4>");
+              $("#winterval").removeClass("hidden");
+              break;
+            case actions.GAME_START:
+              if(!$("#wstart").hasClass("hidden"))
+                $("#wstart").addClass("hidden");
+
+              $("#winterval").addClass("hidden");
+              $("#wgame").removeClass("hidden");
+              $("#wscores").removeClass("hidden");
+              break;
+            case actions.RESULT:
+              $("#wgame").addClass("hidden");
+              var r = "";
+
+              if(obj.value.drawn)
+                r = "Drawn";
+              else if(obj.value.won)
+                r = "Won";
+              else if(obj.value.lost)
+                r = "Lost";
+                
+              $("#user-score").html(obj.value.scores.you);
+              $("#other-score").html(obj.value.scores.opp);
+
+              $("#winterval").html("<h4>You have " + r + "</h4>");
+              $("#winterval").removeClass("hidden");
+              $("#wagain").removeClass("hidden");
+              break;
+            case actions.DISCONNECT:
+              $("#wgame").addClass("hidden");
+              $("#wscores").addClass("hidden");
+              $("#wagain").addClass("hidden");
+              
+              $("#user-score").html(0);
+              $("#other-score").html(0);
+              $("#winterval").html("<h4>Other user disconnected, fight abandoned!</h4>");
+              
+              $("#wstart").removeClass("hidden");
+              $("#winterval").removeClass("hidden");
+              break;
+            case actions.MAX_USERS:
+              $("#wgame").addClass("hidden");
+              $("#wscores").addClass("hidden");
+              $("#wagain").addClass("hidden");
+              
+              $("#winterval").html("<h4>Too many players connected!</h4>");
+              
+              $("#wstart").removeClass("hidden");
+              $("#winterval").removeClass("hidden");
+          }
+        };
+        socket.onclose = function(){
+          socket = null;
+          
+          $("#wgame").addClass("hidden");
+          $("#wscores").addClass("hidden");
+          $("#wagain").addClass("hidden");
+          
+          $("#user-score").html(0);
+          $("#other-score").html(0);
+          $("#winterval").html("<h4>You've been disconnected...</h4>");
+          
+          $("#wstart").removeClass("hidden");
+          $("#winterval").removeClass("hidden");
+        };
+
+        sendServer(socket, {
+          action: actions.PLAY
+        });
+      };
+  }
+
+  $("#wstart button").click(function(){
+      $("#winterval").html("Connecting...");
+      $("#wstart").addClass("hidden");
+      if(socket)
+        sendServer(socket, {
+          action: actions.PLAY
+        });
+      else
+        connectServer();
+  });
+  
+  $("#wagain button").click(function() {
+    $("#wagain").addClass("hidden");
+    $("#winterval").html("<h4>Requesting next duel...</h4>");
+    sendServer(socket, {
+      action: actions.PLAY_AGAIN
+    });
+  });
+
+  $("#wselect button").click(function(){
+    var val = $(this).data('val');
+
+    if(socket)
+      sendServer(socket, {value: val, action: actions.SELECT});
+      
+    $("#wgame").addClass("hidden");
+    $("#winterval").html("<h4>Awaiting other user's weapon choice...</h4>");
+    $("#winterval").removeClass("hidden");
+  });
+});
