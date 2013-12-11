e7e71396450bc7a710453cf726059f0211421c6e
Luke Fisher <hysteriax23@gmail.com>
2013-12-11 20:36:58 +0000

    Updated engine.io for polling only

--- static/main.js
+++ static/main.js
@@ -24,7 +24,9 @@ $(document).ready(function(){
 
   function connectServer() {
     var loc = location.hostname;
-      socket = eio(location.protocol == "http:" ? "ws://" : "wss://" + loc);
+      socket = eio(location.protocol == "http:" ? "ws://" : "wss://" + loc, {
+        transports: ['polling']
+      });
       socket.onopen = function(){
         socket.onmessage = function(data){
           var obj = JSON.parse(data);
